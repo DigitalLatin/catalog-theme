2204f3bda5169418f80fcce914535388cd656333
Samuel J. Huskey <samueljhuskey@gmail.com>
2018-08-24 13:53:11 -0500

    Reverting to previous file name.

--- fionta-base-theme/includes/DllNodeFormatter.php
+++ fionta-base-theme/includes/DllNodeFormatter.php
@@ -3,7 +3,7 @@
 require_once dirname(__FILE__) . '/DllAuthorAuthoritiesFormatter.php';
 require_once dirname(__FILE__) . '/DllRepositoryItemFormatter.php';
 require_once dirname(__FILE__) . '/DllWebPageFormatter.php';
-require_once dirname(__FILE__) . '/DllWorkFormatter.php';
+require_once dirname(__FILE__) . '/DllDllWorkFormatter.php';
 
 class DLLNodeFormatter {
   private $node;
